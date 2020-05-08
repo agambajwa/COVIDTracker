cc7fa72d29769de12fbd1cab5c04958d4f7a1196
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-05-08 23:22:38 +0530

    Updated UI and functionality

--- app/src/main/java/com/agambajwa/covidtracker/About.java
+++ app/src/main/java/com/agambajwa/covidtracker/About.java
@@ -3,10 +3,36 @@ package com.agambajwa.covidtracker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.models.BarModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class About extends AppCompatActivity {
+
+    TextView tvLastUpdated;
+    SimpleArcLoader simpleArcLoader;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +43,74 @@ public class About extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        tvLastUpdated = findViewById(R.id.tvLastUpdated);
+        simpleArcLoader = findViewById(R.id.loader);
+        linearLayout = findViewById(R.id.llLastUpdated);
 
+        fetchData();
     }
+    private void fetchData() {
 
+        String url = "https://corona.lmao.ninja/v2/all/";
+        simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    tvLastUpdated.setText("Last updated: " + getDate(jsonObject.getLong("updated")));
+
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    linearLayout.setVisibility(View.VISIBLE);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    linearLayout.setVisibility(View.VISIBLE);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                linearLayout.setVisibility(View.VISIBLE);
+                Toast.makeText(About.this, "Error Occurred! "+ error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == android.R.id.home)
             finish();
         return super.onOptionsItemSelected(item);
+    }
+    private String getDate(long milliSeconds) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy, hh:mm:ss aaa");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    public void openBrowser(View view) {
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 }
